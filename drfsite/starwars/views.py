65fabc626082f6a6ef24e1954a932083bb692ff1
AlexeyTeacher <ak45rus@gmail.com>
2022-04-14 23:23:42 +0300

    add pagination

--- drfsite/starwars/views.py
+++ drfsite/starwars/views.py
@@ -2,6 +2,7 @@ from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
@@ -11,10 +12,17 @@ from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import PersonSerializer
 
 
+class StarwarsAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class StarwarsAPIList(generics.ListCreateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = StarwarsAPIListPagination
 
 
 class StarwarsAPIUpdate(generics.RetrieveUpdateAPIView):
